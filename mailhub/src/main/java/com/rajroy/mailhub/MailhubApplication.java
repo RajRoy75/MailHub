76c98ae5be4457e0effd8b20ab8fd712fad07650
Raj Roy <rajroy894290@gmail.com>
2025-11-08 18:03:22 +0530

    adding .env file

--- mailhub/src/main/java/com/rajroy/mailhub/MailhubApplication.java
+++ mailhub/src/main/java/com/rajroy/mailhub/MailhubApplication.java
@@ -3,11 +3,15 @@ package com.rajroy.mailhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MailhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailhubApplication.class, args);
-	}
+  public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    SpringApplication.run(MailhubApplication.class, args);
+  }
 
 }
